3a341830760fc713762d07ffb385795bf5c30adc
KarsKnook <57411502+KarsKnook@users.noreply.github.com>
2021-06-15 18:22:30 +0100

    Update create_graph.py

--- create_graph.py
+++ create_graph.py
@@ -4,24 +4,34 @@ import random
 
 
 def generate_graph(n=10, a=0, b=1):
-    # Generates complete weighted graph with n nodes and edge weights uniformly distributed between a and b.
-    # Triangle equality does not hold!
-    G = nx.Graph()
+    """
+    Input: n is the number of nodes we want
+    Edge weights are sampled from the uniform distribution U([a,b])
+    Output: G is a complete weighted graph
+    """
+    G = nx.Graph() # create a graph
     edge_list = [(i, j, random.uniform(a, b)) for i in range(n-1) for j in range(i, n)] 
     G.add_weighted_edges_from(edge_list)
     return G
 
 
 def generate_graph_triangle(n=10, a=1, b=1):
-    # Generates complete weighted graph with n nodes with position in [0, a]x[0, b]
-    # Outputs complete weighted graph and a dictionary of positions which is helpful for drawing the graph
-    # Triangle equality does hold!
-    G = nx.Graph()
+    """
+    Input: n is the number of nodes we want
+    [0,a] x [0,b] is the set where the nodes lie within
+    Output: G is a complete weighted graph with n nodes with positions 
+    randomly generated from the uniform distribution U([0, a]x[0, b])
+    with weight equal to the distance between the two nodes
+    the weight is a metric!
+    """
+    G = nx.Graph()  # create a graph
 
-    node_labels = range(n)
+    node_labels = range(n)  # label the n nodes
+    # generate the position coordinates of the nodes
     coordinates = [np.array([random.uniform(0, a), random.uniform(0, b)]) for i in node_labels]
     pos = dict(zip(node_labels, coordinates))
     
+    # define the weight of an edge by the Euclidean distance between the two nodes
     edge_list = [(i, j, np.linalg.norm(coordinates[i] - coordinates[j])) for i in range(n-1) for j in range(i, n)] 
     G.add_weighted_edges_from(edge_list)
 
@@ -29,15 +39,22 @@ def generate_graph_triangle(n=10, a=1, b=1):
 
 
 def generate_graph_triangle_3D(n=10, a=1, b=1, c=1):
-    # Generates complete weighted graph with n nodes with position in [0, a]x[0, b]x[0, c]
-    # Outputs complete weighted graph and a dictionary of positions which is helpful for drawing the graph
-    # Triangle equality does hold!
-    G = nx.Graph()
+    """
+    Input: n is the number of nodes we want
+    [0,a] x [0,b] x [0,c] is the set where the nodes lie within
+    Output: G is a complete weighted graph with n nodes with positions 
+    randomly generated from the uniform distribution U([0, a]x[0, b]x[0,c])
+    with weight equal to the distance between the two nodes
+    the weight is a metric!
+    """
+    G = nx.Graph()  # create a graph
 
-    node_labels = range(n)
+    node_labels = range(n)  # label the n nodes
+    # generate the position coordinates of the nodes
     coordinates = [np.array([random.uniform(0, a), random.uniform(0, b), random.uniform(0, c)]) for i in node_labels]
     pos = dict(zip(node_labels, coordinates))
     
+    # define the weight of an edge by the Euclidean distance between the two nodes
     edge_list = [(i, j, np.linalg.norm(coordinates[i] - coordinates[j])) for i in range(n-1) for j in range(i, n)] 
     G.add_weighted_edges_from(edge_list)
 
